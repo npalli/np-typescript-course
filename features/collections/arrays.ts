b5c9a7f1202f1f53b63aa1303520a355b847a2f9
Nikhil Palli <nikhil.palli@cortland.com>
2021-02-11 15:41:22 -0500

    finished collections

---
+++ features/collections/arrays.ts
@@ -0,0 +1,21 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+//string[][]
+const carsByMake = [['f150'], ['corolla'], ['implala']];
+
+//string[]
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//prevent incompatiable values
+// ! carMakers.push(100); causes error
+
+// help with map
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2021-01-31'];
+importantDates.push('2021-12-20');
---
+++ features/collections/tuple.ts
@@ -0,0 +1,18 @@
+const drink = {
+    colo: 'brown',
+    carbonated: true,
+    sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+
+//assign a type to index of the array
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+
+//tuple vs object
+const carSpecs: [number, number] = [400, 3354];
+const carSpecsObject = {
+    horesepower: 400,
+    weight: 3354,
+};
